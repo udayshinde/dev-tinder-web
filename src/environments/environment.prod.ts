130c6297626cbfe2fec56a5f6ec66e889e7bc608
uday <uday.shinde3188@gmail.com>
2025-06-10 15:24:58 +0530

    base url change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: "https://api.dev-tinder.info/api" // your production backend URL with port
+    apiBaseUrl: "https://dev-tinder.info/api" // your production backend URL with port
 };
