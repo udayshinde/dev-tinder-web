abce7779d973d6dc4994608fdb1d220ea1a0dc02
uday <uday.shinde3188@gmail.com>
2025-06-10 21:06:37 +0530

    s was missing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: "http://api.dev-tinder.info:7777/api" // your production backend URL with port
+    apiBaseUrl: "https://api.dev-tinder.info:7777/api" // your production backend URL with port
 };
