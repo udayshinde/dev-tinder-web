3f87e45b49bbfbf6720ea52ea8da52dd1de71307
uday <uday.shinde3188@gmail.com>
2025-06-24 22:23:08 +0530

    url for prod changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     //apiBaseUrl: "https://api.dev-tinder.info/api" // your production backend URL with port
-    apiBaseUrl: "http://namaste-node-backend:7777/api"
+    apiBaseUrl: "/api"
 };
