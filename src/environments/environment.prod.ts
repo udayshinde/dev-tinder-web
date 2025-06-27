36734c97521695f2af52abf50c73021b96af313a
uday <uday.shinde3188@gmail.com>
2025-06-27 11:04:18 +0530

    api changes for docker-compose

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     //apiBaseUrl: "https://api.dev-tinder.info/api" // your production backend URL with port
-    apiBaseUrl: ""
+    apiBaseUrl: "/api"
 };
